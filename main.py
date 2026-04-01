0bea48adfe69c200d2e40f1507fb73badce43dac
Bilgin Şen <mbilginsen@gmail.com>
2026-04-01 19:12:23 +0300

    Catch the Turtle Project revised

--- main.py
+++ main.py
@@ -14,44 +14,71 @@ turtle_ins.shapesize(2)
 timer_turtle=turtle.Turtle()
 timer_turtle.color("red")
 timer_turtle.hideturtle()
-timer_turtle.teleport(0,500)
+#y=drawing_board.window_height() # küçük ekranda oynamak için kullanılabilir
+#timer_turtle.teleport(0,y/2*0.9)
+timer_turtle.teleport(0,510)
 
 score_turtle=turtle.Turtle()
 score_turtle.color("black")
 score_turtle.hideturtle()
-score_turtle.teleport(0, 470)
+#score_turtle.teleport(0, y/2*0.8) # küçük ekran için kullanılabilir
+score_turtle.teleport(0, 480)
 
-#counter=int(input("Enter how many seconds you want to play: "))
-
-counter=10
+#time=0
 score=0
+gameover=False
+
+'''def time_turtle():
+    global time
+    time_turtle = turtle.Turtle()
+    time_turtle.color("dark blue")
+    time_turtle.hideturtle()
+    time= int(input(time_turtle.write("Enter how many seconds you want to play: ", align="center", font=("Arial", 24, "normal"))))
+    #timer_turtle.write(f"Enter how many seconds you want to play: {time}", align="center", font=("Arial", 24, "normal"))
+    timer_turtle.clear()'''
 
 def my_function(x,y):
     global score
     score += 1
 
-def move_turtle():
-    global counter
+def countdown(time):
+    global gameover
     timer_turtle.clear()
+    timer_turtle.write(f"Time: {time}", align="center", font=("Arial", 24, "normal"))
+    if time > 0:
+        drawing_board.ontimer(lambda: countdown(time-1),1000)
+    else:
+        timer_turtle.clear()
+        timer_turtle.write("Game Over!", align="center", font=("Arial", 24, "normal"))
+        gameover=True
+        turtle_ins.hideturtle()
+
+
+def move_turtle():
     score_turtle.clear()
-    timer_turtle.write(f"Time: {counter}",align="center", font=("Arial", 24, "normal"))
     score_turtle.write(f"Score: {score}", align="center", font=("Arial", 24, "normal"))
     x = random.randint(-960, +960)
     y = random.randint(-540, +540)
     turtle_ins.teleport(x, y)
-    counter -= 1
     turtle_ins.onclick(my_function)
 
     # Set timer for 1000ms (1 second) and recall countdown
-    if counter >= 0:
-        drawing_board.ontimer(move_turtle,1000)
-
-    if counter < 0:
-        timer_turtle.clear()
-        timer_turtle.write("Game Over!", align="center", font=("Arial", 24, "normal"))
+    if not gameover:
+        #turtle_ins.clear()
+        drawing_board.ontimer(move_turtle,1250)
+    else:
         score_turtle.write(f"Score: {score}", align="center", font=("Arial", 24, "normal"))
-        turtle_ins.hideturtle()
+        #turtle_ins.hideturtle()
 
+'''time_turtle()
+
+if time > 0:
+    countdown(time)
+    move_turtle()'''
+
+countdown(10)
 move_turtle()
+
+
 
 turtle.mainloop()
